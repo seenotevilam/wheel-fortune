c97f0cfa3a2c0995aaca9df51afd1adb59db8db8
seenotevil <i.sheludko@petshop.ru>
2023-12-16 03:07:20 +0300

    add static

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+
+app.use(
+    '/css',
+    express.static(`${__dirname}/css/`)
+);
+
+app.use(
+    '/class',
+    express.static(`${__dirname}/class/`)
+);
+
+app.use(
+    '/fonts',
+    express.static(`${__dirname}/fonts/`)
+);
+
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/index.html");
 });
